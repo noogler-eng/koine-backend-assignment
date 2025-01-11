1625fa0faea03aae8660dfdaaef70d2e54f867c2
noogler-eng <sharadpoddar10102003@gmail.com>
2025-01-11 09:13:53 +0530

    node-cron added

--- src/worker/fetchData.ts
+++ src/worker/fetchData.ts
@@ -1,3 +1,41 @@
 import axios from "axios";
 import Coins from "../models/schema";
+import cron from "node-cron";
 
+const fetchData = async () => {
+  // running for every 0 minutes and 2 hour
+  cron.schedule("0 */2 * * *", async () => {
+    try {
+      const response = await axios.get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,matic-network,ethereum&vs_currencies=usd&include_market_cap=true&include_24hr_change=true"
+      );
+      const { bitcoin, "matic-network": matic, ethereum } = response.data;
+
+      const cryptoData = [
+        {
+          name: "Bitcoin",
+          price: bitcoin.usd,
+          marketCap: bitcoin.usd_market_cap,
+          change: bitcoin.usd_24h_change,
+        },
+        {
+          name: "Matic-network",
+          price: matic.usd,
+          marketCap: matic.usd_market_cap,
+          change: matic.usd_24h_change,
+        },
+        {
+          name: "Ethereum",
+          price: ethereum.usd,
+          marketCap: ethereum.usd_market_cap,
+          change: ethereum.usd_24h_change,
+        },
+      ];
+
+      await Coins.insertMany(cryptoData);
+      console.log("working running.");
+    } catch (error) {
+      console.error("Error fetching coins data:", error);
+    }
+  });
+};
